a349e9dee8ae57f0ec49307a5cb5eecbff63d378
Lucas Rodrigues <lucas.darlindo@gmail.com>
2023-08-21 21:28:12 -0300

    feat: Report Output added

--- controller/Venda.py
+++ controller/Venda.py
@@ -31,48 +31,60 @@ class VendaController:
         Método responsável pela emissão do relatório das Vendas realizadas dentro do sistema.
         """
         vendas = self._db.read(target_db="vendas")
-        pessoas_atendidas = []
-        medicamentos_vendidos = []
-        for venda in vendas:
-            produtos_ids = vendas["produto_ids"]
-            produtos = [
-                produto for produto in [
-                    self._db.search(
-                        target_db="medicamentos", target_field="identificador", search_value=prod_id, lazy_mode=False
-                    ) for prod_id in produtos_ids
-                ]
-            ]
+        pessoas_atendidas = set()
+        quimioterapicos_vendidos = []
+        fitoterapicos_vendidos = []
+        for venda in vendas.values():
             if venda.cliente not in pessoas_atendidas:
-                pessoas_atendidas.append(venda.cliente)
+                pessoas_atendidas.add(venda.cliente.cpf)
 
-            for med in produtos:
-                qtdd = 0
-                if med not in medicamentos_vendidos:
-                    medicamentos_vendidos.append({'obj': med, 'qtdd': qtdd, 'valor_total': qtdd * med.valor})
+            for produto in venda.produtos:
+                med = self._db.search(
+                    target_db="medicamentos", target_field="identificador",
+                    search_value=produto.identificador, lazy_mode=False
+                )
+                qtdd = len([m for m in venda.produtos if m.identificador == produto.identificador])
+                if produto.tipo_medicamento == "Q":  # quimio
+                    if med not in [m['obj'] for m in quimioterapicos_vendidos]:
+                        quimioterapicos_vendidos.append({'obj': med, 'qtdd': qtdd, 'valor_total': qtdd * med.valor})
                 else:
-                    i = [m['obj'] for m in medicamentos_vendidos].index(med)
-                    medicamentos_vendidos[i]['qtdd'] += qtdd
-                    medicamentos_vendidos[i]['valor_total'] += qtdd * med.valor
+                    if med not in [m['obj'] for m in fitoterapicos_vendidos]:
+                        fitoterapicos_vendidos.append({'obj': med, 'qtdd': qtdd, 'valor_total': qtdd * med.valor})
 
-        if len(medicamentos_vendidos) > 0:
-            mais_vendido = sorted(medicamentos_vendidos, key=lambda x: x['qtdd'])[-1]
+        if len(fitoterapicos_vendidos) > 0:
+            mais_vendido_f = sorted(fitoterapicos_vendidos, key=lambda x: x['qtdd'])[-1]
         else:
-            mais_vendido = {'obj': None, 'qtdd': 0, 'valor_total': 0}
+            fitoterapicos_vendidos.append({'obj': None, 'qtdd': 0, 'valor_total': 0})
+
+        if len(quimioterapicos_vendidos) > 0:
+            mais_vendido_q = sorted(quimioterapicos_vendidos, key=lambda x: x['qtdd'])[-1]
+        else:
+            quimioterapicos_vendidos.append({'obj': None, 'qtdd': 0, 'valor_total': 0})
+
+        if mais_vendido_f['qtdd'] > mais_vendido_q['qtdd']:
+            mais_vendido = mais_vendido_f
+        else:
+            mais_vendido = mais_vendido_q
 
         print(f""" ========== Relatório Final {datetime.today().strftime("%d/%m/%Y")} ==========\n
-        Medicamento mais vendido:
-        {mais_vendido['obj']}
-        Quantidade: {mais_vendido['qtdd']}
-        Valor Total: R$ {mais_vendido['valor_total']:.2f}
+            Medicamento mais vendido:
+            {mais_vendido['obj']}
+            Quantidade: {mais_vendido['qtdd']}
+            Valor Total: R$ {mais_vendido['valor_total']:.2f}
 
-        * * * * * * * * * * * * * * * * *
+            * * * * * * * * * * * * * * * * *
 
-        Quantidade de pessoas atendidas: {len(pessoas_atendidas)}
+            Quantidade de pessoas atendidas: {len(pessoas_atendidas)}
 
-        * * * * * * * * * * * * * * * * *
+            * * * * * * * * * * * * * * * * *
 
-        Medicamentos vendidos hoje:
-        Quantidade: {sum([m['qtdd'] for m in medicamentos_vendidos])}
-        Valor: R$ {sum([m['valor_total'] for m in medicamentos_vendidos]):.2f}
-        Tipo: 
-        """.replace('.', ','))
+            Quimioterápicos vendidos hoje:
+            Quantidade: {sum([m['qtdd'] for m in quimioterapicos_vendidos])}
+            Valor: R$ {sum([m['valor_total'] for m in quimioterapicos_vendidos]):.2f}
+
+            * * * * * * * * * * * * * * * * *
+
+            Fitoterápicos vendidos hoje:
+            Quantidade: {sum([m['qtdd'] for m in fitoterapicos_vendidos])}
+            Valor: R$ {sum([m['valor_total'] for m in fitoterapicos_vendidos]):.2f}
+            """.replace('.', ','))
